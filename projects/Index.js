375954f9b5127381ba245f56d56ad0affa31735e
Shivendra <67376433+shivendrakc@users.noreply.github.com>
2023-07-08 12:43:55 +1000

    Reverting changes back to normal testing project link

---
+++ projects/Index.js
@@ -0,0 +1,25 @@
+const value = document.getElementById("value");
+const btns = document.querySelectorAll(".btn");
+let count = 0;
+
+btns.forEach((btn) =>{
+    btn.addEventListener("click", (e) =>{
+        const styles = e.currentTarget.classList;
+        if (styles.contains("decrease")){
+            count--;
+        }else if(styles.contains("increase")){
+            count++;
+        }else{
+            count = 0;
+        }
+        if(count < 0 ){
+            value.style.color = "red";
+        }else if(count > 0){
+            value.style.color = "green";
+        }else{
+            value.style.color = "black";
+        }
+        value.textContent = count;
+    })
+})
+
